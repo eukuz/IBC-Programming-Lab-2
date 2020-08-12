61b5118f081c02a2c08a3719e327f521101abd0e
evgn.worker <evgn.worker@gmail.com>
2020-08-12 05:10:32 +0500

    interface for 4,5,6 cases. n case incompleted

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <list>
+#include <sstream>
+#include <string>
 
 bool checkUniqueness(int cities[], int size)
 {
@@ -46,8 +48,8 @@ int getByIndex(std::list<int> cities, int index) {
 		if (i++ == index)
 			return n;
 }
-void setByIndex(std::list<int> cities, int index, int value) {
-	int i = 0;
+void setByIndex(std::list<int>& cities, int index, int value) {
+	int i = 1;
 	for (int n : cities)
 		if (i++ == index)
 		{
@@ -56,17 +58,19 @@ void setByIndex(std::list<int> cities, int index, int value) {
 		}
 }
 
-int aDistance(int distances[], std::list<int> cities, int size) {
-	int res = distances[getByIndex(cities, size - 1) * size + cities.front()];
+int aDistance(std::list <int> distances, std::list<int> cities, int size) {
+	int res = getByIndex(distances, getByIndex(cities, size - 1) * size + cities.front());
 
 	for (int i = 0; i < size - 1; i++)
-		res += distances[getByIndex(cities, i) * size + getByIndex(cities, i + 1)];
+		res += getByIndex(distances, getByIndex(cities, i) * size + getByIndex(cities, i + 1));
 
 	return res;
 }
 
-void tsp_solve_4(int distances[16]) {
+int tsp_solve_4(int distances[16]) {
 	int minDistance = INT_MAX;
+
+
 
 	for (int c2 = 1; c2 <= 3; c2++)
 		for (int c3 = 1; c3 <= 3; c3++)
@@ -78,10 +82,10 @@ void tsp_solve_4(int distances[16]) {
 				if (actualDistance < minDistance)
 					minDistance = actualDistance;
 			}
-	std::cout << minDistance << std::endl;;
+	return minDistance;
 
 }
-void tsp_solve_5(int distances[25]) {
+int tsp_solve_5(int distances[25]) {
 
 	int minDistance = INT_MAX;
 
@@ -96,18 +100,18 @@ void tsp_solve_5(int distances[25]) {
 					if (actualDistance < minDistance)
 						minDistance = actualDistance;
 				}
-	std::cout << minDistance << std::endl;
+	return minDistance;
 }
 
-void tsp_solve_6(int distances[25]) {
+int tsp_solve_6(int distances[36]) {
 
 	int minDistance = INT_MAX;
 
-	for (int c2 = 1; c2 <= 6; c2++)
-		for (int c3 = 1; c3 <= 6; c3++)
-			for (int c4 = 1; c4 <= 6; c4++)
-				for (int c5 = 1; c5 <= 6; c5++)
-					for (int c6 = 1; c6 <= 6; c6++)
+	for (int c2 = 1; c2 <= 5; c2++)
+		for (int c3 = 1; c3 <= 5; c3++)
+			for (int c4 = 1; c4 <= 5; c4++)
+				for (int c5 = 1; c5 <= 5; c5++)
+					for (int c6 = 1; c6 <= 5; c6++)
 					{
 						int cities[6] = { 0,c2,c3,c4,c5,c6 };
 						if (!checkUniqueness(cities, 6)) continue;
@@ -115,7 +119,7 @@ void tsp_solve_6(int distances[25]) {
 						if (actualDistance < minDistance)
 							minDistance = actualDistance;
 					}
-	std::cout << minDistance << std::endl;
+	return minDistance ;
 }
 
 int sumCities(std::list<int> cities) {
@@ -130,7 +134,7 @@ void IncreaseCities(std::list<int>& cities, int size) {
 	while (i != 1)
 	{
 		int value = getByIndex(cities, i);
-		if (value != size-1) {
+		if (value != size - 1) {
 			setByIndex(cities, i, value + 1);
 			return;
 		}
@@ -139,9 +143,9 @@ void IncreaseCities(std::list<int>& cities, int size) {
 	}
 }
 
-void tsp_solve_n(int distances[], int size) {
+void tsp_solve_n(std::list<int> distances, int size) {
 
-	
+
 	int minDistance = INT_MAX;
 
 	std::list<int> cities;
@@ -149,14 +153,15 @@ void tsp_solve_n(int distances[], int size) {
 	for (int i = 1; i < size; i++)
 		cities.push_back(1);
 
-	while (cities.front()!= 1)
+	while (cities.front() != 1)
 	{
-		if (!checkUniqueness(cities, size)) continue;
-		int actualDistance = aDistance(distances, cities, 6);
-		if (actualDistance < minDistance)
-			minDistance = actualDistance;
-
-		IncreaseCities(cities,size);
+		if (checkUniqueness(cities, size))
+		{
+			int actualDistance = aDistance(distances, cities, 6);
+			if (actualDistance < minDistance)
+				minDistance = actualDistance;
+		}
+		IncreaseCities(cities, size);
 	}
 
 	std::cout << minDistance << std::endl;
@@ -176,21 +181,70 @@ int main()
 	//    {8,3,0,1,},
 	//    {6,7,1,0,}
 	//        };
-	int distances4[16] = { 0,10,8,6, 10,0,3,7, 8,3,0,1, 6,7,1,0 };
-	int distances5[25] = { 0,10,8,6,1, 10,0,3,7,2, 8,3,0,1,3, 6,7,1,0,4, 1,2,3,4,0 };
+	//int distances4[16] = { 0,10,8,6, 10,0,3,7, 8,3,0,1, 6,7,1,0 };
+	//int distances5[25] = { 0,10,8,6,1, 10,0,3,7,2, 8,3,0,1,3, 6,7,1,0,4, 1,2,3,4,0 };
+	//int distances6[36] = { 0,10,8,6,1,1, 10,0,3,7,2,2, 8,3,0,1,3,3, 6,7,1,0,4,4, 1,2,3,4,0,5, 1,2,3,4,5,0 };
+
+	std::cout << "Inappropriate n\n";
 
 	std::cout << "Pls input the number of cities: ";
 	int size = 0;
 	std::cin >> size;
-	std::cout << "Pls input the matrix ("<<size<<" numbers by line separated with a space): ";
-	for (size_t i = 0; i < size; i++)
-	{
+	//std::cout << "Pls input the matrix ("<<size<<" numbers separated with a space in " << size <<" lines):\n";
+	std::cout << "Pls input the matrix " << size * size << " numbers one per line):\n";
+	std::list<int> distancesN;
 
+	//for (int i = 0; i < size * size; i++)
+	//{
+	//	/*std::string input = "";
+	//	std::cin >> input;
+	//	for (int j = 0; j < size; j++)
+	//	{
+	//		std::istringstream iss(input);
+	//		for (int k; iss >> k; )
+	//			;*/
+	//			//}
+	//	int newValue;
+	//	std::cin >> newValue;
+	//	;		distancesN.push_back(newValue);
+
+	//}
+
+	int newValue;
+	int result;
+
+	switch (size)
+	{
+		case 4:
+			int distances4[16];
+			for (int i = 0; i < 16; i++)
+			{ 
+				std::cin >> newValue;
+				distances4[i] = newValue;
+			}
+			result = tsp_solve_4(distances4);
+		break;
+		case 5:
+			int distances5[25];
+			for (int i = 0; i < 25; i++)
+			{
+				std::cin >> newValue;
+				distances5[i] = newValue;
+			}
+			result = tsp_solve_5(distances5);
+			break;
+		case 6:
+			int distances6[36];
+			for (int i = 0; i < 36; i++)
+			{
+				std::cin >> newValue;
+				distances6[i] = newValue;
+			}
+			result = tsp_solve_6(distances6);
+			break;
 	}
 
-
-	tsp_solve_4(distances4);
-	tsp_solve_5(distances5);
+	std::cout << "The shortest way costs: "<<result;
 
 }
 
